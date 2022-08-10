d0cfe77cbc392bc6691e7e6416a550f9a3c8f930
symB10S <jonathandotmeerholz@gmail.com>
2022-08-10 09:06:29 +0200

    Removed Refelction information form wavefront clas

--- Main_Algorithm/Merge_Cartesian.py
+++ Main_Algorithm/Merge_Cartesian.py
@@ -323,13 +323,6 @@ def Process_Wavefronts(Inductor_List, Capacitor_List, Circuit_List):
         excitation_event_type = Decimal()
         excitation_event_number = Decimal()
 
-        reflections_inductive = Decimal()
-        reflections_capacitive = Decimal()
-        refelections_current_chain = Decimal()
-
-        transmisions_inductor_to_capacitor = Decimal()
-        transmisions_capacitor_to_inductor = Decimal()
-
         def about(self) :
             print("\nSome Information about a wavefront")
             print(f"{'Type :':<35}{type(self)}")
@@ -341,11 +334,6 @@ def Process_Wavefronts(Inductor_List, Capacitor_List, Circuit_List):
             print(f"{'Current Magnitude :':<35}{self.magnitude_current}")
             print(f"{'Excitation Type :':<35}{'HIGH' if self.excitation_event_type else 'LOW' }")
             print(f"{'Excitation Number :':<35}{self.excitation_event_number}")
-            print(f"{'Inductive Reflections :':<35}{self.reflections_inductive}")
-            print(f"{'Capacitive Reflections :':<35}{self.reflections_capacitive}")
-            print(f"{'Current Chain Reflections :':<35}{self.refelections_current_chain}")
-            print(f"{'Transmission Ind -> Cap :':<35}{self.transmisions_inductor_to_capacitor}")
-            print(f"{'Transmission Cap -> Ind :':<35}{self.transmisions_capacitor_to_inductor}")
         
         def Time_at_position(self,position):
             x = Decimal(position)
@@ -388,20 +376,13 @@ def Process_Wavefronts(Inductor_List, Capacitor_List, Circuit_List):
 
             self.excitation_event_number = excitation_number
 
-            self.reflections_capacitive = 0
-            self.reflections_inductive = 0
-            self.refelections_current_chain = 0
-
-            self.transmisions_inductor_to_capacitor = 0
-            self.transmisions_capacitor_to_inductor = 0
-
         def Generate(self, Wavefront_Storage : list):
             Wavefront_Storage.append(Wavefront_Inductive(self,False))
             Wavefront_Storage.append(Wavefront_Capacitive(self,False))
             
     class Wavefront_Capacitive( Wavefront ):
 
-        def __init__(self, Wavefront_Parent : Wavefront, is_reflection : bool, reflections_current_chain = 0):
+        def __init__(self, Wavefront_Parent : Wavefront, is_reflection : bool):
             
             self.velocity = Capacitor_Velocity
             self.length = Capacitor_Length
@@ -420,12 +401,6 @@ def Process_Wavefronts(Inductor_List, Capacitor_List, Circuit_List):
                     self.magnitude_voltage = Circuit_Solver_Capacitor_Voltage(0, 0, Wavefront_Parent.magnitude_voltage, Wavefront_Parent.magnitude_current)
                     self.magnitude_current = Circuit_Solver_Capacitor_Current(0, 0, Wavefront_Parent.magnitude_voltage, Wavefront_Parent.magnitude_current)
 
-                    self.reflections_inductive = Wavefront_Parent.reflections_inductive
-                    self.reflections_capacitive = Wavefront_Parent.reflections_capacitive + 1
-
-                    self.transmisions_inductor_to_capacitor = Wavefront_Parent.transmisions_inductor_to_capacitor
-                    self.transmisions_capacitor_to_inductor = Wavefront_Parent.transmisions_capacitor_to_inductor
-
                 elif isinstance(Wavefront_Parent, Wavefront_Source) : # A generate source wave (Vs)-|->
 
                     self.time_start = Wavefront_Parent.time_start
@@ -434,22 +409,10 @@ def Process_Wavefronts(Inductor_List, Capacitor_List, Circuit_List):
                     self.magnitude_voltage = Circuit_Solver_Capacitor_Source_Voltage(Wavefront_Parent.magnitude_voltage)
                     self.magnitude_current = Circuit_Solver_Capacitor_Source_Current(Wavefront_Parent.magnitude_voltage)
 
-                    self.reflections_inductive = Wavefront_Parent.reflections_inductive
-                    self.reflections_capacitive = Wavefront_Parent.reflections_capacitive 
-
-                    self.transmisions_inductor_to_capacitor = Wavefront_Parent.transmisions_inductor_to_capacitor
-                    self.transmisions_capacitor_to_inductor = Wavefront_Parent.transmisions_capacitor_to_inductor
-
                 else: # A transmitted wave at source side  -|->
 
                     self.magnitude_voltage = Circuit_Solver_Capacitor_Voltage(Wavefront_Parent.magnitude_voltage, Wavefront_Parent.magnitude_current, 0, 0)
                     self.magnitude_current = Circuit_Solver_Capacitor_Current(Wavefront_Parent.magnitude_voltage, Wavefront_Parent.magnitude_current, 0, 0)
-
-                    self.reflections_inductive = Wavefront_Parent.reflections_inductive
-                    self.reflections_capacitive = Wavefront_Parent.reflections_capacitive 
-
-                    self.transmisions_inductor_to_capacitor = Wavefront_Parent.transmisions_inductor_to_capacitor + 1
-                    self.transmisions_capacitor_to_inductor = Wavefront_Parent.transmisions_capacitor_to_inductor
 
             else: # A reflected wave at load side   -->|
 
@@ -458,29 +421,21 @@ def Process_Wavefronts(Inductor_List, Capacitor_List, Circuit_List):
                 self.magnitude_voltage = Wavefront_Parent.magnitude_voltage
                 self.magnitude_current = - Wavefront_Parent.magnitude_current
 
-                self.reflections_inductive = Wavefront_Parent.reflections_inductive
-                self.reflections_capacitive = Wavefront_Parent.reflections_capacitive + 1
-
-                self.transmisions_inductor_to_capacitor = Wavefront_Parent.transmisions_inductor_to_capacitor
-                self.transmisions_capacitor_to_inductor = Wavefront_Parent.transmisions_capacitor_to_inductor
-
             self.excitation_event_type = Wavefront_Parent.excitation_event_type
             self.excitation_event_number = Wavefront_Parent.excitation_event_number
-
-            self.refelections_current_chain = reflections_current_chain
 
         def Generate(self, Wavefront_Storage):
             if self.position_end == 0:
                 Wavefront_Storage.append(Wavefront_Inductive(self,False))
                 Wavefront_Storage.append(Wavefront_Capacitive(self,True))
             else:
-                Wavefront_Storage.append(Wavefront_Capacitive(self,True,self.refelections_current_chain+1))
+                Wavefront_Storage.append(Wavefront_Capacitive(self,True))
         
         def Generate_Return(self):
             if self.position_end == 0:
                 return Wavefront_Inductive(self,False), Wavefront_Capacitive(self,True)
             else :
-                return Wavefront_Capacitive(self,True,self.refelections_current_chain+1)
+                return Wavefront_Capacitive(self,True)
 
         def Merge(self, Wavefront_Other : Wavefront):
             self.magnitude_voltage = self.magnitude_voltage + Wavefront_Other.magnitude_voltage
@@ -488,7 +443,7 @@ def Process_Wavefronts(Inductor_List, Capacitor_List, Circuit_List):
 
     class Wavefront_Inductive( Wavefront ):
 
-        def __init__(self, Wavefront_Parent : Wavefront, is_reflection : bool, reflections_current_chain = 0):
+        def __init__(self, Wavefront_Parent : Wavefront, is_reflection : bool):
             
             self.velocity = Inductor_Velocity
             self.length = Inductor_Length
@@ -507,12 +462,6 @@ def Process_Wavefronts(Inductor_List, Capacitor_List, Circuit_List):
                     self.magnitude_voltage = Circuit_Solver_Inductor_Voltage( Wavefront_Parent.magnitude_voltage, Wavefront_Parent.magnitude_current, 0, 0)
                     self.magnitude_current = Circuit_Solver_Inductor_Current( Wavefront_Parent.magnitude_voltage, Wavefront_Parent.magnitude_current, 0, 0)
 
-                    self.reflections_inductive = Wavefront_Parent.reflections_inductive + 1
-                    self.reflections_capacitive = Wavefront_Parent.reflections_capacitive 
-
-                    self.transmisions_inductor_to_capacitor = Wavefront_Parent.transmisions_inductor_to_capacitor
-                    self.transmisions_capacitor_to_inductor = Wavefront_Parent.transmisions_capacitor_to_inductor
-
                 elif isinstance(Wavefront_Parent, Wavefront_Source) : # A generate source wave (Vs)-|->
 
                     self.time_start = Wavefront_Parent.time_start
@@ -520,23 +469,11 @@ def Process_Wavefronts(Inductor_List, Capacitor_List, Circuit_List):
 
                     self.magnitude_voltage = Circuit_Solver_Inductor_Source_Voltage(Wavefront_Parent.magnitude_voltage)
                     self.magnitude_current = Circuit_Solver_Inductor_Source_Current(Wavefront_Parent.magnitude_voltage)
-
-                    self.reflections_inductive = Wavefront_Parent.reflections_inductive
-                    self.reflections_capacitive = Wavefront_Parent.reflections_capacitive 
-
-                    self.transmisions_inductor_to_capacitor = Wavefront_Parent.transmisions_inductor_to_capacitor
-                    self.transmisions_capacitor_to_inductor = Wavefront_Parent.transmisions_capacitor_to_inductor
                     
                 else: # A transmitted wave at source side  -|->
 
                     self.magnitude_voltage = Circuit_Solver_Inductor_Voltage(0, 0, Wavefront_Parent.magnitude_voltage, Wavefront_Parent.magnitude_current)
                     self.magnitude_current = Circuit_Solver_Inductor_Current(0, 0, Wavefront_Parent.magnitude_voltage, Wavefront_Parent.magnitude_current)
-
-                    self.reflections_inductive = Wavefront_Parent.reflections_inductive
-                    self.reflections_capacitive = Wavefront_Parent.reflections_capacitive 
-
-                    self.transmisions_inductor_to_capacitor = Wavefront_Parent.transmisions_inductor_to_capacitor 
-                    self.transmisions_capacitor_to_inductor = Wavefront_Parent.transmisions_capacitor_to_inductor +1
 
             else: # A reflected wave at load side   -->|
                 
@@ -545,29 +482,21 @@ def Process_Wavefronts(Inductor_List, Capacitor_List, Circuit_List):
                 self.magnitude_voltage = - Wavefront_Parent.magnitude_voltage
                 self.magnitude_current = Wavefront_Parent.magnitude_current
 
-                self.reflections_inductive = Wavefront_Parent.reflections_inductive +1
-                self.reflections_capacitive = Wavefront_Parent.reflections_capacitive 
-
-                self.transmisions_inductor_to_capacitor = Wavefront_Parent.transmisions_inductor_to_capacitor
-                self.transmisions_capacitor_to_inductor = Wavefront_Parent.transmisions_capacitor_to_inductor
-
             self.excitation_event_type = Wavefront_Parent.excitation_event_type
             self.excitation_event_number = Wavefront_Parent.excitation_event_number
-
-            self.refelections_current_chain = reflections_current_chain
 
         def Generate(self, Wavefront_Storage):
             if self.position_end == 0:
                 Wavefront_Storage.append(Wavefront_Inductive(self,True))
                 Wavefront_Storage.append(Wavefront_Capacitive(self,False))
             else :
-                Wavefront_Storage.append(Wavefront_Inductive(self,True,self.refelections_current_chain+1))
+                Wavefront_Storage.append(Wavefront_Inductive(self,True))
 
         def Generate_Return(self):
             if self.position_end == 0:
                 return Wavefront_Inductive(self,True), Wavefront_Capacitive(self,False)
             else :
-                return Wavefront_Inductive(self,True,self.refelections_current_chain+1)
+                return Wavefront_Inductive(self,True)
 
         def Merge(self, Wavefront_Other : Wavefront):
             self.magnitude_voltage = self.magnitude_voltage + Wavefront_Other.magnitude_voltage
